393169731794d20a036597ddd2b6e75dddfeb92b
Kanisk <57884127+chakrakan@users.noreply.github.com>
2020-05-29 00:40:12 -0400

    wow

---
+++ src/tripletexting/tripletexting.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  string input;
+  string leftPass, rightPass;
+  cin >> input;
+  int wordLen = input.size() / 3;
+  leftPass = input.substr(0, wordLen);
+  rightPass = input.substr(input.size()-wordLen, input.size());
+  if (leftPass == rightPass) {
+      cout << leftPass << endl;
+  } else {
+      cout << input.substr(wordLen, wordLen) << endl;
+  }
+}
